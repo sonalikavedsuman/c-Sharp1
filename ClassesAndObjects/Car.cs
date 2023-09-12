5171f600039524cf9f83b15a95c00855ecad231b
Sonalika <sonalika011@gmail.com>
2023-09-13 00:04:48 +0530

    Add project files.

---
+++ ClassesAndObjects/Car.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ClassesAndObjects
+{
+    internal class Car
+    {
+        //Member Variables
+        //Access Modifier --> private
+        //private string _name;
+        private string _name;
+        private int _hp;
+        private string _color;
+        
+        // Default Constructor
+
+        public Car()
+        {
+            _name = "Car";
+            _hp = 0;
+            _color = "Red";
+        }
+
+        //Partial Specification Constructor
+        public Car(string name, int hp = 0)
+        {
+            _name = name;
+            Console.WriteLine(name + " was created");
+            _hp = hp;
+            _color = "red";
+
+        }
+
+        //Full Specification Constructor
+        public Car(string name, int hp, string color)
+        {
+            _name = name;
+            Console.WriteLine(name + " was created");
+            _hp = hp;
+            _color = color;
+
+        }
+
+        //Constructor
+        //public Car(string name, int hp = 0 , string color = "Black") 
+        //{
+        //    _name = name;
+        //    Console.WriteLine(name + " was created");
+        //    _hp = hp;
+        //    _color = color;
+
+        //}
+
+        //Member Method
+        public void Drive() 
+        {
+            Console.WriteLine(_name + " is Driving");
+        }
+        public void Stop() 
+        {
+            Console.WriteLine(_name + " Stopped");
+        }
+        public void Details()
+        {
+            Console.WriteLine("My Car name is " + _name + " " +_color + " Color and It has horse power of " + _hp);
+        }
+    }
+}
---
+++ ClassesAndObjects/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Details();
+            Car audi = new Car("Audi A4", 350 ,"Blue");
+            audi.Drive();
+            audi.Details();
+            Car bmw = new Car("BMW M5", 450);
+            bmw.Drive();
+            bmw.Details();
+            Console.WriteLine("Press 1 to stop the car");
+            string userInput = Console.ReadLine();
+            if(userInput == "1")
+            {
+                audi.Stop();
+            }
+            else
+            {
+                Console.WriteLine("Car drives indefinitely");
+            }
+            
+            Console.Read();
+
+        }
+    }
+}
